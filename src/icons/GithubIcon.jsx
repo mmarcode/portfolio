ce674be63c91610b58b2dacc9d487f6d8b68bcf9
mmarco <mmiranda@next-cloud.mx>
2025-04-27 13:48:26 -0600

    change link to github

--- src/icons/GithubIcon.jsx
+++ src/icons/GithubIcon.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
 const GithubIcon = (props) => (
-    <a href="https://github.com/mmarcode" target="_blank" rel="noopener noreferrer"
-        aria-label="mmarcode Github"
+    <a href="https://github.com/mmarcode/portfolio" target="_blank" rel="noopener noreferrer" aria-label="Github mmarcode"
         className="w-9 h-9 flex items-center justify-center rounded-lg text-neutral-800 dark:text-neutral-100
         hover:bg-gray-400/20 dark:hover:bg-gray-100/10 transition-all duration-200 cursor-pointer" {...props}>
         <i className="bx bxl-github text-xl"></i>
